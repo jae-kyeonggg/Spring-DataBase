b1df6f610fded7f1028362a75fc12a60f75939df
jae-kyeonggg <hanjaekyung56@gmail.com>
2023-11-24 00:25:01 +0900

    e.g. UnChecked Exception

---
+++ src/test/java/hello/jdbc/exception/basic/UncheckedTest.java
@@ -0,0 +1,54 @@
+package hello.jdbc.exception.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class UncheckedTest {
+
+    @Test
+    void unchecked_catch() {
+        Service service = new Service();
+        service.callCatch();
+    }
+
+    @Test
+    void unchecked_throw() {
+        Service service = new Service();
+        Assertions.assertThatThrownBy(() -> service.callThrow())
+                .isInstanceOf(MyUncheckedException.class);  //also works with RuntimeException.class
+    }
+
+    /*
+     * The exception that extends java.lang.RunTimeException -> Unchecked Exception
+     */
+    static class MyUncheckedException extends RuntimeException {
+        public MyUncheckedException(String message) {
+            super(message);
+        }
+    }
+
+    static class Service {
+        Repository repository = new Repository();
+
+        public void callCatch() {
+            try {
+                repository.call();
+            } catch (MyUncheckedException e) {
+                log.info("Exception Handling, message={}", e.getMessage(), e);
+            }
+        }
+
+        public void callThrow() {
+            repository.call();
+        }
+    }
+
+    static class Repository {
+        public void call() {
+            throw new MyUncheckedException("ex");
+        }
+    }
+
+}
